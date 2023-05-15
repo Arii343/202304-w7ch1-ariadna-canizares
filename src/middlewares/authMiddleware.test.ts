56b500e3d6afffa7c6c88c113d5a3f33bb0729ac
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-15 23:53:33 +0200

    Add authMiddleware and Add authMiddleware tests

---
+++ src/middlewares/authMiddleware.test.ts
@@ -0,0 +1,57 @@
+import { type Request, type Response } from "express";
+import jwt from "jsonwebtoken";
+import { auth } from "./authMiddleware";
+import CustomError from "../CustomError/CustomError";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given n auth middleware", () => {
+  const token = "token";
+
+  const req: Pick<Request, "header"> = {
+    header: jest.fn().mockReturnValue(`Bearer ${token}`),
+  };
+
+  const res = {};
+  const next = jest.fn();
+
+  describe("When it receives an authorization header with a valid token and a next function", () => {
+    test("Then it should call the received next function", () => {
+      jwt.verify = jest.fn();
+
+      auth(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an authorization header with an invalid token and a next function", () => {
+    test("Then it should call the received next function with a 401 'Invalid token' error", () => {
+      const expectedError = new CustomError(401, "Invalid token");
+
+      jwt.verify = jest.fn().mockImplementation(() => {
+        throw expectedError;
+      });
+
+      auth(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+
+  describe("When it receives an authorization header without bearer and a next function", () => {
+    test("Then it should call the received next function with a 401 'Missing token'", () => {
+      const req: Pick<Request, "header"> = {
+        header: jest.fn().mockReturnValue(token),
+      };
+
+      const expectedError = new CustomError(401, "Missing token");
+
+      auth(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
